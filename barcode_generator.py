17e8fb8b9d5e8b2f3e23586bb0c06f931131f162
dinamitrii <k_karaivanov@abv.bg>
2023-08-27 13:08:07 +0300

    Full Module

--- barcode_generator.py
+++ barcode_generator.py
@@ -6,8 +6,4 @@ for figure_one in range(int(first_number[0]), int(second_number[0]) + 1):
         for figure_three in range(int(first_number[2]), int(second_number[2]) + 1):
             for figure_four in range(int(first_number[3]), int(second_number[3]) + 1):
                 if figure_one % 2 != 0 and figure_two % 2 != 0 and figure_three % 2 != 0 and figure_four % 2 != 0:
-                    print(f"{figure_one}{figure_two}{figure_three}{figure_four}",end= " ")
-
-
-
-
+                    print(f"{figure_one}{figure_two}{figure_three}{figure_four}", end=" ")
